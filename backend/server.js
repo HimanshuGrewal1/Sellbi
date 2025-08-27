c45053f97d6634e91daee9a7a8c7ce8cc70894cb
HimanshuGrewal1 <g.himanshu2806@gmail.com>
2025-08-27 18:19:29 +0530

    basic Auth

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./src/DB/connectMongoDB.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+import authRoutes from "./src/routes/auth.route.js";
+
+const app = express();
+dotenv.config()
+
+
+const port=process.env.PORT || 8000
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.get("/",(req,res)=>{
+    res.send("Server is ready")
+})
+
+app.use("/api/auth", authRoutes);
+
+
+ connectDB().then(()=>{
+    app.listen(port,()=>{
+    console.log(`Server is runing at port ${port}`)
+   
+
+})}
+
+ )
